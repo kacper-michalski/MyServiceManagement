227686b8d9d7c6136edbf0d0f6e5cce33015349a
Kacper Michalski <Kacper.Michalski250@gmail.com>
2022-09-26 20:54:08 +0200

    Updated add-client component

--- src/app/services/add-client.service.ts
+++ src/app/services/add-client.service.ts
@@ -16,7 +16,21 @@ export class AddClientService {
     }
   }
 
-  public addClient(client: ClientDetails) {
+  public addClient(client: any) {
+    console.log(client);
+
+
+    const ids: string = client.addressesId.map((obj: { addressId: string; }) => obj.addressId);
+    client.addressesId.splice(0);
+    console.log(ids)
+    // for (let id of ids) {
+    //   console.log(client.addressesId)
+    //   console.log(id)
+    //   client.addressId.push(id);
+
+    // }
+    // console.log(client.addressesId);
+    client.addressesId.push(ids)
     console.log(client);
     return this.http.post<ClientDetails>('http://localhost:8080/customer', client);
   }
--- src/app/views/add-client/add-client.component.ts
+++ src/app/views/add-client/add-client.component.ts
@@ -8,7 +8,16 @@ import { DropDown } from 'src/app/models/dropDown.model';
 import { Company } from 'src/app/models/company.model';
 import { Address } from 'src/app/models/address.model';
 import { AddAddressService } from 'src/app/services/add-address.service';
+import { AddCompanyService } from 'src/app/services/add-company.service';
 
+interface address1 {
+  name: string,
+  id: string
+}
+interface company1 {
+  name: string,
+  tin: string
+}
 @Component({
   selector: 'app-add-client',
   templateUrl: './add-client.component.html',
@@ -16,42 +25,48 @@ import { AddAddressService } from 'src/app/services/add-address.service';
 })
 export class AddClientComponent {
   companies: Company[];
-  addressesMOCK: Address[];
+  addressesMOCK: address1[];
+  companiesMOCK: company1[];
   selectedAddress: string;
-  defaultAddress={
-    streetNumber: [""],
-    town: [""],
-    zip: [""]
+  selectedCompany: string;
+  defaultAddress = {
+    addressId: [""],
   };
   clientForm = this.fb.group({
     firstName: [this.getClientDetails().firstName, [Validators.required,]],
     lastName: [this.getClientDetails().lastName, [Validators.required,]],
     phoneNumber: [this.getClientDetails().phoneNumber, [Validators.required, Validators.minLength(9)]],
     email: [this.getClientDetails().email, [Validators.required, Validators.email]],
-    addresses: this.fb.array([this.fb.group({
-      streetNumber: [this.getAddress().streetNumber, [Validators.required]],
-      town: [this.getAddress().town, [Validators.required]],
-      zip: [this.getAddress().zip, [Validators.required]]
-    })])
+    // addresses: this.fb.array([this.fb.group({
+    //   streetNumber: [this.getAddress().streetNumber, [Validators.required]],
+    //   town: [this.getAddress().town, [Validators.required]],
+    //   zip: [this.getAddress().zip, [Validators.required]]
+    // })])
+    addressesId: this.fb.array([this.fb.group({
+      addressId: [this.getAddress().streetNumber, [Validators.required]]
+    })]),
+    tin: [this.getCompany().tin, [Validators.required]]
   });
-  constructor(private fb: FormBuilder, private addClientService: AddClientService, public showDropDownService: ShowDropDownService, private addAddressService: AddAddressService) {
-    this.companies = [
-      { name: 'optica ', tin: '8378245074', street: 'Pana Tadeusza 9', zipCode: '02-964', city: 'Rzeszów' },
-      { name: 'ukom', tin: '1259726332', street: 'Europejska 94', zipCode: '20-609', city: 'Warszawa' },
-      { name: 'sllegro ', tin: '8114516680', street: 'Bałtycka 12', zipCode: '90-215', city: 'Bydgoszcz' }
+  constructor(private fb: FormBuilder, private addClientService: AddClientService, public showDropDownService: ShowDropDownService, private addAddressService: AddAddressService, private addCompanyService: AddCompanyService) {
+    this.companiesMOCK = [
+      { name: 'optica ' + '\n' + '8378245074' + '\n' + 'Pana Tadeusza 9' + '\n' + '02-964' + '\n' + 'Rzeszów', tin: '1' },
+      { name: 'ukom' + '\n' + '1259726332' + '\n' + 'Europejska 94' + '\n' + '20-609' + '\n' + 'Warszawa', tin: '2' },
+      { name: 'sllegro ' + '\n' + '8114516680' + '\n' + 'Bałtycka 12' + '\n' + '90-215' + '\n' + 'Bydgoszcz', tin: '3' }
     ],
-    this.addressesMOCK = [
-      { streetNumber: 'Szkolna 17', zip: '69-997', town: 'Białystok', devices: ['farelka', 'pralka', 'odkurzacz', 'mikrofala'] },
-      { streetNumber: 'Szkolna 15', zip: '42-605', town: 'Białystok', devices: ['spawarka'] },
-      { streetNumber: 'Boboli 8', zip: '44-164', town: 'Białystok', devices: ['kuchenka gazowa'] },
-      { streetNumber: 'Boboli 10', zip: '84-230', town: 'Białystok', devices: ['skoda'] },
-    ]
-   }
+      this.addressesMOCK = [
+        { name: 'Centralna 54' + '\n' + '31-521 Białystok', id: '1' },
+        { name: 'Koszalińska 39' + '\n' + '42-605 Kraków', id: '2' },
+        { name: 'Beliny Prażmowskiego 6' + '\n' + '44-164 Radom', id: '3' },
+        { name: 'Sikorskiego Władysława 144' + '\n' + '84-230 Katowice', id: '4' },
+      ]
+  }
 
   private getClientDetails() {
     return this.addClientService.clientDetails;
   }
-
+  private getCompany(){
+    return this.addCompanyService.company;
+  }
 
   public onSubmit() {
     const client = this.clientForm.value as ClientDetails;
@@ -70,9 +85,9 @@ export class AddClientComponent {
     return this.addAddressService.address;
   }
   public get addresses() {
-    return (this.clientForm.get('addresses')! as FormArray<FormGroup>).controls;
+    return (this.clientForm.get('addressesId')! as FormArray<FormGroup>).controls;
   }
-  public addField(path : string, group : any){
+  public addField(path: string, group: any) {
     const fb = this.clientForm.get(path);
     (fb as FormArray).push(this.fb.group(group));
   }
